25fc54dd9f86fda01c616034e94f74ed6b9dd69b
seanlipps <slipps@ucsd.edu>
2026-03-09 21:50:30 -0700

    moved traceback to gpu

--- nussinov.cuh
+++ nussinov.cuh
@@ -17,11 +17,24 @@
 #define HD inline
 #endif
 
+// cell index
+struct cell_ind{
+  int x;
+  int y;
+};
+
 // Convert to triangular indexing
 HD int triInd(int i, int j, int N) {
   int index = (N-MIN_LOOP_LENGTH-1)*i-((i)*(i+1))/2+j-MIN_LOOP_LENGTH-1;
 
   return index;
+}
+
+HD int triInd_safe(int i, int j, int N, const int* DP) {
+    if (j - i <= MIN_LOOP_LENGTH) {
+        return 0; 
+    }
+    return DP[triInd(i, j, N)];
 }
 
 #ifdef __CUDACC__
@@ -36,4 +49,4 @@ HD bool pair_check(const uint8_t* seq, int i, int j) {
   return (nuc1 ^ nuc2) == 3;
 }
 
-void nussinov_gpu_wrap(uint8_t* seq, int* DP, int N);
+void nussinov_gpu_wrap(uint8_t* seq, cell_ind* structure, int* trace_len, int N);
